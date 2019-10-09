5f80425f62f1983b35a8195cb0d545b6e1c7584b
bharatbhatt-agli <54534229+bharatbhatt-agli@users.noreply.github.com>
2019-10-09 12:09:24 +0530

    first_connect _with_git

---
+++ XLRelease/check_plugins/abc.groovy
@@ -0,0 +1,34 @@
+// Exported from:        http://DESKTOP-RFIQ2A0:5516/#/templates/Foldera193ffce62c443439b6057fe8132d37b-Release7e799da780884a12a5170baed6eff0e6/releasefile
+// XL Release version:   9.0.4
+// Date created:         Wed Oct 09 12:09:21 IST 2019
+
+xlr {
+  template('abc') {
+    folder('check_plugins')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-10-07T09:00:00+0530')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('jh') {
+            
+          }
+          manual('bbbb') {
+            
+          }
+          manual('bll') {
+            
+          }
+        }
+      }
+    }
+    releaseTriggers {
+      gitPoll('git_trigger') {
+        releaseTitle 'trigger_func2'
+        tags 'git'
+        gitRepository 'git_server'
+        branch 'master'
+      }
+    }
+    
+  }
+}
